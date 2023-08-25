f93a92befee1ae731d15bd6314a0e606aac2fc75
922tech <108605086+922tech@users.noreply.github.com>
2023-08-26 02:40:06 +0330

    Update tasks.py

--- apps/delay/tasks.py
+++ apps/delay/tasks.py
@@ -1,9 +1,7 @@
 from celery import shared_task
 import time
-from django.shortcuts import redirect
 from django.http import HttpResponse
 
 @shared_task
 def answer_late():
     time.sleep(5)
-    return redirect("https://www.google.com")
