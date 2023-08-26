3b7434046b583d52fb1280090a11a31f8d059979
922tech <922tech@gmail.com>
2023-08-26 11:53:36 +0430

    edition in authentication

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,34 +1,48 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.views.decorators.csrf import csrf_exempt
 from config import settings
 from .services import AuthService
+ 
 
-
+BROWSER_AGENT =  'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36'
 client_id = settings.OAUTH_CREDENTIALS['login']['client_id']
 client_secret = settings.OAUTH_CREDENTIALS['login']['client_secret']
 token_url = settings.TOKEN_URL
 
 
 def index(request):
-    return HttpResponse('<h1>THIS APP</h1>')
+    return HttpResponse('<h1>Welcome to the web app</h1>')
 
 
-def login(request:HttpRequest):
+@api_view(['GET','POST'])
+def login(request):
     if request.method == 'GET':
         return render(request, 'login.html')
 
     if request.method == 'POST':
+        user_agent = request.headers.get('user-agent')
         if request.COOKIES.get('access_token'):
             return HttpResponse(f'<section>You are already logged in</section>')
+        if user_agent == BROWSER_AGENT :
+            
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            print(request.POST.dict())
+            credentials = {"username":username, "password":password}
+        else:
+            credentials = request.data
 
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        response =  AuthService.obtain_token_request(username, password)
+        response =  AuthService.obtain_token_request(**credentials)
 
         if response.status_code == 200:
             response_data = response.json()
+
+            if user_agent != BROWSER_AGENT :
+                return Response(response_data)
+
             access_token = response_data["access_token"]
             refresh_token = response_data["refresh_token"]
             res =  HttpResponse(f'<section>You have logged in!</section>')
@@ -41,18 +55,27 @@ def login(request:HttpRequest):
         return res
 
 
-def logout(request :HttpRequest):
+@csrf_exempt
+@api_view(['GET','POST'])
+def logout(request):
     if request.method == 'GET':
         return render(request, 'logout.html')
     
     elif request.method == 'POST':
-        token = request.COOKIES.get('access_token')
-        if not token:
-            return HttpResponse(f'<section>you are not logged in.</section>' )
+        user_agent = request.headers.get('user-agent')
+
+        if user_agent == BROWSER_AGENT:
+            token = request.COOKIES.get('access_token')
+            if not token:
+                return HttpResponse(f'<section>you are not logged in.</section>' , status=400)
+            
+        else:
+            token = request.data['token']
+            if not token:
+                Response({'response':'You are not logged in'}, status=400)
             
         response = AuthService.revoke_token_request(token)
-        res = HttpResponse(f'<section>{response.status_code}</section>' )
-        print( response.text)
-        return res
+        print(response.text)
+
+        return HttpResponse(f'<h3> Good Bye! </h3>' , status=200)
     
-    pass
