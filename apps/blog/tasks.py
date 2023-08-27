bc15a98887a2b994b77dc0260cb5d7587d3ede62
922tech <922tech@gmail.com>
2023-08-27 11:10:13 +0430

    debugging

--- apps/blog/tasks.py
+++ apps/blog/tasks.py
@@ -5,11 +5,11 @@ import time
 from typing import Union
 
 @shared_task
-def increment_post_view(post_obj: Union[QuerySet, int]):
+def increment_post_view(post_obj: Union[QuerySet, str]):
     """Increases the number of views on a post"""
     time.sleep(2)
 
-    if not isinstance(post_obj, int):
+    if isinstance(post_obj, str):
         post_obj = Post.objects.filter(id=post_obj)
 
     post_obj.update(views=F("views") + 1)
